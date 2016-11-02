f73ac4da7083d90b33d63aa11223e8cda4b1dba4
iotzov <ivaniotzov@gmail.com>
2016-11-02 15:20:02 -0400

    fix processing script and add stat tracker

--- displayRaw.m
+++ displayRaw.m
@@ -1,10 +1,15 @@
-files = dir('dataMatFiles/*.mat');
+% Run settings script to set up environment variables
+settings_local
 
+% Get all mat files in raw data directory
+files = dir([RAW_DATA_PATH '*.mat']);
+
+% Loop through all files, preprocessing and displaying
 for i=1:length(files)
-    load(['dataMatFiles/' files(i).name]);
+    load([RAW_DATA_PATH files(i).name]);
     fs = eegData.srate;
     x = eegData.data(1:37,:)';
-    xclean = preprocess(x,33:37,{[]},fs);
+    xclean = preprocessEEG(x,33:37,[],fs);
     imagesc(xclean'); caxis([-100 100]);
     title(files(i).name);
     pause
--- preprocessEEG.m
+++ preprocessEEG.m
@@ -12,41 +12,41 @@ HPcutoff =0.5; % HP filter cut-off frequequency in Hz
 % pick your preferred high-pass filter
 [b,a,k]=butter(5,HPcutoff/fs*2,'high'); sos = zp2sos(b,a,k);
 
-[T,D,N]=size(X); 
+[T,D]=size(X); 
 
-% Preprocess data for all N subjects
-for i=1:N
-    
-    data = X(:,:,i);
 
-    % remove starting offset to avoid filter trancient
-    data = data-repmat(data(1,:),T,1);
-    
-    % show the original data
-    if debug, subplot(2,1,1); imagesc((1:T)/fs,1:D,data'); title(['Subject ' num2str(i)]); end
+% Preprocess data
 
-    % high-pass filter
-    data = sosfilt(sos,data);          
-    
-    % regress out eye-movements;
-    data = data - data(:,eogchannels) * (data(:,eogchannels)\data); 
+data = X(:,:);
 
-    % detect outliers above stdThresh per channel; 
-    data(abs(data)>stdThresh*repmat(std(data),[T 1])) = NaN;
-    
-    % remove 40ms before and after;
-    h=[1; zeros(round(0.04*fs)-1,1)];    
-    data = filter(h,1,flipud(filter(h,1,flipud(data))));
-    
-    % Mark outliers as 0, to avoid NaN coding and to discount noisy channels
-    data(isnan(data))=0;
+% remove starting offset to avoid filter transient
+data = data-repmat(data(1,:),T,1);
 
-    % also zero out bad channels
-    data(:,badchannels{i})=0; 
-    
-    % show the result of all this
-    if debug, subplot(2,1,2); imagesc((1:T)/fs,1:D,data'); caxis([-100 100]); xlabel('Time (s)'); drawnow; end
+% show the original data
+if debug, subplot(2,1,1); imagesc((1:T)/fs,1:D,data'); title('work in progress'); end
 
-    X(:,:,i) = data;
+% high-pass filter
+data = sosfilt(sos,data);          
+
+% regress out eye-movements;
+data = data - data(:,eogchannels) * (data(:,eogchannels)\data); 
+
+% detect outliers above stdThresh per channel; 
+data(abs(data)>stdThresh*repmat(std(data),[T 1])) = NaN;
+
+% remove 40ms before and after;
+h=[1; zeros(round(0.04*fs)-1,1)];    
+data = filter(h,1,flipud(filter(h,1,flipud(data))));
+
+% Mark outliers as 0, to avoid NaN coding and to discount noisy channels
+data(isnan(data))=0;
+
+% also zero out bad channels
+data(:,badchannels(:))=0; 
+
+% show the result of all this
+if debug, subplot(2,1,2); imagesc((1:T)/fs,1:D,data'); caxis([-100 100]); xlabel('Time (s)'); drawnow; end
+
+X(:,:) = data;
     
 end
