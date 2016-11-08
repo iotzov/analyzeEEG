1b3df892c0770c99598d4a3c99fbcaff55f78418
iotzov <ivaniotzov@gmail.com>
2016-11-08 16:28:55 -0500

    add display raw data function

---
+++ displayRawEEG.m
@@ -0,0 +1,21 @@
+function displayRawEEG(eegData, fs)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function displays raw EEG data with the minimum amount of processing required for viewing
+% 	The only processing performed is high-pass filtering and offset removal in order to facilitate viewing
+%
+%
+% Input:
+% 	eegData - MxN array of M samples by N electrodes
+% 	fs - sampling rate in Hz
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[b,a,k]=butter(5,0.5/fs*2,'high'); sos = zp2sos(b,a,k);
+
+[T,D]=size(eegData);                                              
+
+eegData = eegData-repmat(eegData(1,:),T,1);  % remove starting offset to avoid filter transient
+
+eegData = sosfilt(sos,eegData);
+
+imagesc(eegData'); caxis([-30 30]);
