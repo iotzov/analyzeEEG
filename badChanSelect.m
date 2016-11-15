5784c3d50e6104bb9a3fee82caddba3c07aad870
Ivan Iotzov <ivaniotzov@gmail.com>
2016-11-15 00:46:36 -0500

    make bad chan select better

--- badChanSelect.m
+++ badChanSelect.m
@@ -13,24 +13,28 @@ function [badChannels] = badChanSelect(inputEEG)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-eegSize = size(inputEEG);
+eegSize = size(inputEEG.fwd);
 
-badChannels = zeros(1, eegSize(2))
+badChannels = []; removemore=1;
 
 clf
-imagesc(inputEEG'); h1=gca; caxis([-100 100]); set(h1,'xtick',[]); 
-title(['Select bad channels']);
+imagesc(inputEEG.fwd'); h1=gca; caxis([-100 100]); set(h1,'xtick',[]);
+title(['Select bad channels' ' ' inputEEG.name]);
 
-h2=axes('position',[0.1 0.03 0.8 0.05]); x=[-2 -1 0]; imagesc(x, 1, x); 
+h2=axes('position',[0.1 0.03 0.8 0.05]); x=[-2 -1 0]; imagesc(x, 1, x);
 set(h2,'xtick',x,'xticklabel',{'good','OK','bad'},'ytick',[]);
 title('Click on quality to continue.');
 axes(h1);
-
+while(removemore)
 pos=ginput(1);
 
 if pos(1)>0
-    badChannels(indx) = round(pos(2));
+    badChannels = [badChannels round(pos(2))];
 else
-    removemore=0;
-    dataquality(v,indx) = round(abs(pos(1)));
+  removemore=0;
+    dataquality = round(abs(pos(1)));
 end
+
+end
+
+close
