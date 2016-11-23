7f89c701714ebd08a6645cf0978cc8ebae586936
iotzov <ivaniotzov@gmail.com>
2016-11-23 15:59:27 -0500

    reorganize and remove some old files

--- settings_local.m
+++
@@ -1,5 +0,0 @@
-% Set the path to the raw, unprocessed data
-RAW_DATA_PATH = '/home/ivan/Documents/Research Docs /minConsciousEEG/dataMatFiles/'
-
-% Set path to where process data is/will be stored
-PROCESSED_DATA_PATH = '/home/ivan/Documents/Research Docs /minConsciousEEG/processed-mats/'
