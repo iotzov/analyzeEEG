f62cafd4a9c641af9b585f083c27676f78adf307
iotzov <ivaniotzov@gmail.com>
2016-12-19 16:29:09 -0500

    adding the regression

--- ISC/patientRegression.m
+++ ISC/patientRegression.m
@@ -1,12 +1,16 @@
-load ../processedDataWithInfo.mat
+load /home/ivan/Documents/'Research Docs '/minConsciousEEG/processedDataWithInfo.mat
+
+l = length(eeg); lf = 37038; lb = 37040; lt = lf+lb; % define lengths of fwd and bwd segments & total length
+f = matfile('/home/ivan/Documents/Research Docs /minConsciousEEG/regressY.mat');
 
 healthy = find([dataInfo.healthy]==1); patient = find([dataInfo.healthy]==0);
 
-Y = []; X = [];
+Y = []; X = []; Wf = []; Wb = [];
 
-for i = healthy
+for i = 2:3
 
-  Y = cat(1, Y, eeg(i).fwd);
-  Y = cat(1, Y, eeg(i).bwd);
-
+	X = repmat(eeg(i).fwd, [l-1 1]);
+	Y = f.Y;
+	Y(lt*(i-1)+1:lt*i, :) = [];
+	Wf = X\Y;
 end
